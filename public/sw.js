72ffa35402a2a7b8b9d63f2748a0b9addaf5286f
dedkyut <72896457+dedkyut@users.noreply.github.com>
2020-11-09 15:10:14 +0700

    php artisan serve

---
+++ public/sw.js
@@ -0,0 +1,31 @@
+const cacheVersion = 'pwa-test';
+
+const filesToCache = 
+	[
+	'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap',
+	'index.php'
+	];
+
+self.addEventListener('install', function(event) {
+	console.info('Event: Install');
+	event.waitUntil(
+		caches.open(cacheVersion)
+			.then(function(cache) {
+				return cache.addAll(filesToCache)
+			})
+	)
+});
+
+self.addEventListener('fetch', function(event) {
+	console.info('Event: Fetch');
+	event.respondWith(
+		caches.open(cacheVersion).then(function(cache) {
+			return cache.match(event.request).then(function (response) {
+				return response || fetch(event.request).then(function(response) {
+					cache.put(event.request, response.clone());
+					return response;
+				});
+			});
+		})
+	);
+});
